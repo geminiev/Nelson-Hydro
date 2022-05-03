48fef2a922dc704f113bfbcfca4f2775ebecdedb
Eve <71302463+geminiev@users.noreply.github.com>
2022-05-03 09:29:10 -0700

    VBA prompt of Python script

---
+++ gmail_to_outlook.py
@@ -0,0 +1,15 @@
+# program to email people from my Nelson email when prompted by VBA macro
+
+import win32com.client as win32
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'esankar@nelson.ca'
+mail.Subject = 'Message subject'
+mail.Body = 'Message body'
+mail.HTMLBody = '<h2>HTML Message body</h2>' #this field is optional
+
+# To attach a file to the email (optional):
+#attachment  = "Path to the attachment"
+#mail.Attachments.Add(attachment)
+print('hello')
+mail.Send()
